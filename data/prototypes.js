667dcd7b4b4940125c0738fd8d98e46e0ba588f2
dknoma <djknoma@gmail.com>
2020-05-10 13:35:11 -0700

    addressing error in x and y values

--- data/prototypes.js
+++ data/prototypes.js
@@ -1,18 +1,18 @@
 /**
- * Homework 3 Visualization
- * Category: Parking Enforcement
- * Count: 157
+ * Final Steam Data
  */
+const svg = d3.select("body").select("svg#sunburst");
+const width = parseInt(svg.style("width"));
+const height = parseInt(svg.style("height"));
 
-const svg = d3.select("body").select("svg#viz");
-const width = svg.width;
-const height = svg.height;
+console.log(svg);
+console.log('width=%s, height=%s', width, height);
 
 const pad = 14;
 const diameter = 500;
 const radius = width / 2;
 
-let color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1));
+// let color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1));
 let format = d3.format(",d");
 let arc = d3.arc()
             .startAngle(d => d.x0)
@@ -22,55 +22,118 @@ let arc = d3.arc()
             .innerRadius(d => d.y0 * radius)
             .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1));
 
+function partition(data) {
+  const root = d3.hierarchy(data);
+                 // .sum(d => d.value)
+                 // .sort((a, b) => b.value - a.value);
+  return d3.partition()
+           .size([2 * Math.PI, root.height + 1])
+           (root);
+}
+
 d3.json('steam_reduced_with_genre.json')
   .then(draw);
 
-function draw(dat) {
-  let json = d3.hierarchy(dat);
-  console.log(json);
+function draw(data) {
+  let color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1));
 
-  let data = json.data;
-  console.log(data);
+  let root = partition(data);
+  console.log("root");
+  console.log(root);
 
-  // sort larger valued nodes first
-  // data.sort(function(a, b) {
-  //   return b.height - a.height || b.count - a.count;
-  // });
+  root.each(d => d.current = d);
 
-  // size of layout will be width and height minus some padding
-  // let layout = d3.cluster()
-  //                .size([width - 2 * pad, height - 2 * pad])
-  //                (json);
+  const svg = d3.create("svg")
+                .attr("viewBox", [0, 0, width, width])
+                .style("font", "10px sans-serif");
 
-  // call layout to calculate (x, y) locations of nodes and sizes
-  // layout(data);
+  const g = svg.append("g")
+               .attr("transform", `translate(${width / 2},${width / 2})`);
 
-  // setup svg width and height
-  // let svg = d3.select(svg)
-  //             .style("width", width)
-  //             .style("height", height);
+  const path = g.append("g")
+                .selectAll("path")
+                .data(root.descendants().slice(1))
+                .join("path")
+                .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
+                .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
+                .attr("d", d => arc(d.current));
 
-  // shift (0, 0) a little bit to leave some padding
-  // let plot = svg.append("g")
-  //               .attr("id", "plot")
-  //               .attr("transform", translate(pad, pad));
+  path.filter(d => d.children)
+      .style("cursor", "pointer")
+      .on("click", clicked);
 
-  // data.links() are all of the edges as an array
-  // drawLinks(plot.append("g"), data.links(), straightLine);
+  // path.append("title")
+  //     .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
 
-  // data.descendants() are all of the nodes as an array
-  // drawNodes(plot.append("g"), data.descendants(), true);
+  const label = g.append("g")
+                 .attr("pointer-events", "none")
+                 .attr("text-anchor", "middle")
+                 .style("user-select", "none")
+                 .selectAll("text")
+                 .data(root.descendants().slice(1))
+                 .join("text")
+                 .attr("dy", "0.35em")
+                 .attr("fill-opacity", d => +labelVisible(d.current))
+                 .attr("transform", d => labelTransform(d.current))
+                 .text(d => d.data.name);
 
-  // return svg.node();
+  const parent = g.append("circle")
+                  .datum(root)
+                  .attr("r", radius)
+                  .attr("fill", "none")
+                  .attr("pointer-events", "all")
+                  .on("click", clicked);
+
+  function clicked(p) {
+    parent.datum(p.parent || root);
+
+    root.each(d => d.target = {
+      x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+      x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+      y0: Math.max(0, d.y0 - p.depth),
+      y1: Math.max(0, d.y1 - p.depth)
+    });
+
+    const t = g.transition().duration(750);
+
+    // Transition the data on all arcs, even the ones that aren’t visible,
+    // so that if this transition is interrupted, entering arcs will start
+    // the next transition from the desired position.
+    path.transition(t)
+        .tween("data", d => {
+          const i = d3.interpolate(d.current, d.target);
+          return t => d.current = i(t);
+        })
+        .filter(function(d) {
+          return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+        })
+        .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+        .attrTween("d", d => () => arc(d.current));
+
+    label.filter(function(d) {
+      return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+    }).transition(t)
+         .attr("fill-opacity", d => +labelVisible(d.target))
+         .attrTween("transform", d => () => labelTransform(d.current));
+  }
+
+  function arcVisible(d) {
+    return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+  }
+
+  function labelVisible(d) {
+    return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+  }
+
+  function labelTransform(d) {
+    const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+    const y = (d.y0 + d.y1) / 2 * radius;
+    return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+  }
+
+  return svg.node();
 }
 
-function partition(data) {
-  return d3.partition()
-           .size([2 * Math.PI, radius])
-           (d3.hierarchy(data)
-              .sum(d => d.value)
-              .sort((a, b) => b.value - a.value));
-}
 //
 // function drawLinks(g, links, generator) {
 //   let paths = g.selectAll('path')
